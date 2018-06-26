107cca6dfedd583966b1aa2af29460946636a0fc
maryangela <angelamdp16@gmail.com>
2018-06-26 12:32:03 +0200

    ggggggggg

--- resources/views/club/listar_club.blade.php
+++ resources/views/club/listar_club.blade.php
@@ -9,9 +9,8 @@
 <div class="form-row">
   <div class="form-group col-md-12 form-inline">
     <h1>Lista de Clubs:</h1>
-    <a href="club/create/" class="btn btn-primary">Agregar Club</a>
+    <a href="club/create/" class="btn btn-primary" data-toggle="modal" data-target="#modal">Agregar Club</a>
   </div>
-  
 </div>
   <table class="table table-condensed">
       <thead>
@@ -22,10 +21,9 @@
         <th>Ciudad</th>
         <th>Descripcion</th>
         <th>Acciones</th>
-        
       </thead>
-      <tbody>
 
+      <tbody>
         @foreach($clubs as $club)
           <tr>
             <td>{{ $club->id_club}}</td>            
@@ -34,12 +32,41 @@
             <td>{{ $club->nombre." ".$club->apellidos}}</td>
             <td>{{ $club->ciudad}}</td>
             <td>{{ $club->descripcion_club}}</td>
-
             <td><a href="{{ route('club.edit',$club->id_club) }}" class="btn btn-info">Editar</a></td>
             <td><a href="{{ route('club.destroy',$club->id_club) }}" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span>Eliminar</a></td>     
   				</tr>
   			@endforeach
-        
   		</tbody>
 	</table>
+
+
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 @endsection
--- resources/views/fases/list_fase.blade.php
+++ resources/views/fases/list_fase.blade.php
@@ -11,6 +11,63 @@
     <a href="{{ route('fase.create',[$id_disc,$id_gestion]) }}" class="btn btn-primary">Agregar Fase</a>
   </div> 
 </div>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalFase">Agregar</button>
+
+<div class="modal fade" id="modalFase" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Agregar fase</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            {!! Form::label('nombre', 'Nombre', []) !!}
+      {!! Form::text('nombre', null, ['class'=>'form-control','placeholder'=>'Nombre']) !!}
+          </div>
+          <div class="form-group">
+            {!! Form::label('tipo', 'Tipos', []) !!}
+      <br>
+      <div class="card">
+        <div class="card-body">
+          <div class="form-row">
+            
+              @foreach ($tipos as $tipo)
+              <div class="form-group col-md-4">
+                {!! Form::<!DOCTYPE html>
+                <html>
+                <head>
+                  <title></title>
+                </head>
+                <body>
+                
+                </body>
+                </html>radio('tipo',$tipo->id_tipo,null,['id'=>'series','class'=>'radio']) !!}
+                {!! Form::label('series',$tipo->nombre_tipo, []) !!}
+                </div>
+              @endforeach       
+          </div>  
+          </div>
+        </form>
+      </div>
+      <div>
+        <div style="display: none;">
+        {!! Form::text('id_disc', $id_disc, []) !!}
+        {!! Form::text('id_gestion', $id_gestion, []) !!}
+  </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 	<table class="table table-condensed">
   		<thead>
   			<th width="50px">ID</th>

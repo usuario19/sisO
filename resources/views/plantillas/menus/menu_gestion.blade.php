c0ab3aa524826c431d817a79ebf7a143ee2685e5
maryangela <angelamdp16@gmail.com>
2018-08-27 14:52:38 -0400

    falta corregit

--- resources/views/plantillas/menus/menu_gestion.blade.php
+++ resources/views/plantillas/menus/menu_gestion.blade.php
@@ -7,14 +7,6 @@
 @section('submenu')
 <div class="form-row">
 	<div class="form-group col-md-4">
-		{{-- {!! Form::label('gestiones', 'Gestiones', []) !!}
-		{!! Form::select('gestiones', $gestiones, null, ['href'=>"{{ route('gestion.configurar',$gestion->id_gestion) }}",'id'=>'gestiones','class'=>'form-control']) !!} --}}
-		<select name="gestiones" id="gestiones" onchange="location=this.value">
-			@foreach ($gestiones as $gest)
-				<option value="{{ route('gestion.configurar',$gest->id_gestion) }}"> {{ $gest }}
-			@endforeach
-			
-		</select>
 	</div>
 </div>
 
